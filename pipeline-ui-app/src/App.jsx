92fcadddd246478071f5b3bd9aa99f2e8365f103
Electra <electra@bu.edu>
2026-04-21 14:13:33 -0400

    Update App.jsx
    syntax fix

--- pipeline-ui-app/src/App.jsx
+++ pipeline-ui-app/src/App.jsx
@@ -379,7 +379,6 @@ export default function App() {
                 onEditInputs: () => setCelloViewMode("inputs"),
                 isRunning,
               })}
-              />
             />
           </section>
 
