a4f027ae1884d14dd638c0f3e218e84c09377a3a
Electra <electra@bu.edu>
2026-04-21 20:31:45 -0400

    Update xgboost_model.py
    Adding the top rule retrieval

--- dataio/ml/models/xgboost_model.py
+++ dataio/ml/models/xgboost_model.py
@@ -12,8 +12,24 @@ def train(X_train, y_train, X_test, y_test):
     model.fit(X_train, y_train)
 
     preds = model.predict(X_test)
+    
+    ## Retrieves top features from the XGB Model
+    booster = model.get_booster()
+    booster.feature_names = list(feature_names)
+    importance = booster.get_score(importance_type="weight")
+    importance_df = pd.DataFrame({
+        "feature": list(importance.keys()),
+        "importance": list(importance.values())
+    })
+    importance_df = importance_df.sort_values(
+        by="importance",
+        ascending=False
+    )
+    top_n = importance_df.head(rules_N)
 
+    # Returns model, its accuracy, and the top rules dataframe
     return {
         "model": model,
-        "accuracy": accuracy_score(y_test, preds)
+        "accuracy": accuracy_score(y_test, preds),
+        "top_n_rules":top_n
     }
